51dc0937e63498fe3b1be20849cbd0bd4121c765
Aliminesa <97962006+Aliminesa@users.noreply.github.com>
2022-01-27 13:16:43 +0100

    Update Syntax_Gruppenarbeit_180122.R
    Hallo zusammen,
    Versuch Nummer 2, da ich das mit dem Speichern gestern Abend irgendwie verpeilt hab :/ Hier noch einmal ein detaillierterer Überblick über alle meiner Meinung nach für die CA interessanten Variablen. Am Besten währe es wahrscheinlich, das Clustering nach dem Beruf des Haupteinkommensbeziehers des Haushalts auszurichten, oder?
    Ansonsten würde ich heute Abend mal noch ein bisschen experimentieren und verschiedene CAs ausprobieren.
    VG

--- Syntax_Gruppenarbeit_180122.R
+++ Syntax_Gruppenarbeit_180122.R
@@ -1,44 +1,20 @@
-# ok lets go 
-x <- c("1","2","3")
-# Test test
-
-# Also dann los gehts
-# was passiert denn, wenn ich hier editiere - könnt ihr das sehen?
-# Y/N?
-
-# Antwort von Ronja/Alisa:
-# alternativ Github-Kommentar????????????
-
-# Y
-# Hallo Simon, wie es aussieht hat es ja wunderbar geklappt, vielen Dank Dir und sorry für die späte Reaktion!
-
-
-
-# Gruppenarbeit Gruppe 6 
+##############################################
+# Gruppenarbeit CSS, USL Gruppe 6
+##############################################
 # vorgelegt von Alisa Naumann, Ronja Plendl, Simon Hübner
 
-# Datensatz: Mirkozensus 2010
+# Datensatz: Mikrozensus 2010
 # Explorative Untersuchung Berufsgruppen und Lebensumstände // hierarchische Clusteranalyse (+Hauptkomponentenanalye/PCA?)
+# Es geht um die Berufe der Haupteinkommensbezieher eines Haushalts und deren Wohnsituation
 
-
-#install.packages('descr')
-#install.packages('ggplot2')
-#install.packages("stargazer")
-#install.packages('factoextra')
-#install.packages('FactoMineR')
-#install.packages('corrplot')
-#install.packages('datasets')
-#install.packages('dotwhisker')
-
-
-library('descr')  
-library('ggplot2')
-library('stargazer')
-library('factoextra')
-library('FactoMineR')
-library("corrplot")
-library('datasets')
-library ('dotwhisker')
+#library('descr')  
+#library('ggplot2')
+#library('stargazer')
+#library('factoextra')
+#library('FactoMineR')
+#library("corrplot")
+#library('datasets')
+#library ('dotwhisker')
 
 
 setwd("F:/Gruppenarbeit Dateien/Gruppenarbeit_2022_Projekt/Data/")
@@ -46,16 +22,61 @@ getwd()
 
 install.packages('haven')
 library(haven)
-Zensus <- read_dta('mz2010_cf.dta')
+Mikrozensus <- read_dta('mz2010_cf.dta')
+
+#Subdatensatz mit (eventuell) relevanten Variablen ### Eventuell nochmal anpassen an tatsächlich verwendete Variablen
+
+MZsubCA <-Mikrozensus[,c("ef1", "ef136", "ef137", "ef830", "ef739", "ef737", "ef742", "ef707", "ef217", "ef492",
+                          "ef638", "ef663", "ef669", "ef770", "ef667", "ef310", "ef312", "ef44", "ef46", "ef763")]
+
+# einzelne Variablen betrachten
+install.packages('summarytools')
+library(summarytools)
+
+freq(MZsubCA$ef136) 
+freq(MZsubCA$ef739) # Clusteranalyse lieber auf Haupteinkommensbezieher beziehen? Eigentlich sinnvoller, oder?
+
+###### VARIABLEN #########################################
+#LandD <- Mikrozensus$ef1
+
+#### BEFRAGTER
+#Beruf <- Mikrozensus$ef136
+#Wirtschaftszweig <- Mikrozensus$ef137
+#Einkommen <- Mikrozensus$ef830
+
+#hSchulabschluss <- Mikrozensus$ef310
+#hAbschluss <- Mikrozensus$ef312
+#Geschlecht <- Mikrozensus$ef44
+
+#Alter <- Mikrozensus$ef46
+#Arbeitszeit <- Mikrozensus$ef217          #sehr sehr viel NA (98%), würde ich rausnehmen
+
+#### HAUPTEINKOMMENSBEZIEHER
+#BerufHEB <- Mikrozensus$ef739
+#WirtschaftszweigHEB <- Mikrozensus$ef737
+#NettoeinkommenHEB <- Mikrozensus$ef742
+
+#hSchulabschlussHEB <- Mikrozensus$ef743
+#hAbschlussHEB <- Mikrozensus$ef745
+#GeschlechtHEB <- Mikrozensus$ef731
+
+#AlterHEB <- Mikrozensus$????. ###??? # find ich nix zu - brauchen wir das?
+
+#### HAUSHALT
+#Haushaltseinkommen <- Mikrozensus$ef707
+#Wohnraumgröße <- Mikrozensus$ef492
+#QuadratmeterMiete <- Mikrozensus$ef638.   # 57% NA - trotzdem interessant?
+#Haushaltsgröße <- Mikrozensus$ef663
+#AnzahlKinderHH <- Mikrozensus$ef669
+#AnzahlKinderInsges <- Mikrozensus$ef770
+#AnzahlEinkommensbezieher <- Mikrozensus$ef667
+#Lebensformenkonzept <- Mikrozensus$ef736
 
 
-# Beruf (136) (oder Wirtschaftszweig (137))
-# Einkommen (830), bzw. Haushaltseinkommen (707)
-# Arbeitszeit (217)
-# Wohnraumgröße (492)
-# Quadratmeter-Miete (638)
-# Haushaltsgröße (663)
-# Anzahl Kinder im Haushalt (u18, u27 und ü27?) (669)
-# Anzahl Einkommensbezieher (667)
-# höchster Bildungsabschluss (540)
-# (+ Geschlecht (44), Alter (46))
+
+
+
+
+
+
+
