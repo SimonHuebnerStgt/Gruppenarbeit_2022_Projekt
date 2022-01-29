7e4433268d05a3be12637e76bc64fd2db004ee0a
Aliminesa <97962006+Aliminesa@users.noreply.github.com>
2022-01-29 17:46:59 +0100

    Noch ein Update

--- Syntax_Gruppenarbeit_180122.R
+++ Syntax_Gruppenarbeit_180122.R
@@ -3,79 +3,117 @@
 ##############################################
 # vorgelegt von Alisa Naumann, Ronja Plendl, Simon Hübner
 
-# Datensatz: Mikrozensus 2010
+# Datensatz: Mirkozensus 2010
 # Explorative Untersuchung Berufsgruppen und Lebensumstände // hierarchische Clusteranalyse (+Hauptkomponentenanalye/PCA?)
 # Es geht um die Berufe der Haupteinkommensbezieher eines Haushalts und deren Wohnsituation
 
-#library('descr')  
-#library('ggplot2')
-#library('stargazer')
-#library('factoextra')
-#library('FactoMineR')
-#library("corrplot")
-#library('datasets')
-#library ('dotwhisker')
+library('descr')  
+library('ggplot2')
+library('stargazer')
+library('factoextra')
+library('FactoMineR')
+library("corrplot")
+library('datasets')
+library ('dotwhisker')
+library('summarytools')
 
 
-setwd("F:/Gruppenarbeit Dateien/Gruppenarbeit_2022_Projekt/Data/")
+setwd("/Users/alisanaumann/Desktop/Gruppenarbeit_CSS")
 getwd()
 
 install.packages('haven')
 library(haven)
 Mikrozensus <- read_dta('mz2010_cf.dta')
 
+
 #Subdatensatz mit (eventuell) relevanten Variablen ### Eventuell nochmal anpassen an tatsächlich verwendete Variablen
 
-MZsubCA <-Mikrozensus[,c("ef1", "ef136", "ef137", "ef830", "ef310", "ef312", "ef44", "ef46", "ef131", "ef739",
-                          "ef737", "ef742", "ef743", "ef745", "ef707", "ef492", "ef638", "ef663", "ef669", "ef770", "ef667", "ef736")]
+MZsubCA <-Mikrozensus[,c( "ef1",                                                                       #"ef136", "ef310", "ef312", "ef44", "ef46", "ef131",
+                         "ef739", "ef742", "ef745", "ef731", "ef734",
+                         "ef707", "ef492", "ef638", "ef663", "ef669", "ef770", "ef667")]
 
-# einzelne Variablen betrachten
+str(MZsubCA)
+
+#### Befragter
+#MZsubCA$Beruf                     <- MZsubCA$ef136
+#MZsubCA$Einkommen                 <- MZsubCA$ef830 
+#MZsubCA$Schulabschluss            <- MZsubCA$ef310
+#MZsubCA$hoechsterAbschluss        <- MZsubCA$ef312 
+#MZsubCA$Geschlecht                <- MZsubCA$ef44 
+#MZsubCA$Alter                     <- MZsubCA$ef46 
+
+#### Haupteinkommensbezieher
+MZsubCA$Land                      <- MZsubCA$ef1
+
+MZsubCA$BerufHEB                  <- MZsubCA$ef739
+MZsubCA$EinkommenHEB              <- MZsubCA$ef742
+MZsubCA$AbschlussHEB              <- MZsubCA$ef745
+MZsubCA$GeschlechtHEB             <- MZsubCA$ef731
+MZsubCA$StaatsangehörigkeitHEB    <- MZsubCA$ef734
+
+#### Haushalt
+MZsubCA$Haushaltseinkommen        <- MZsubCA$ef707 
+MZsubCA$Wohnraumgroeße            <- MZsubCA$ef492 
+MZsubCA$QuadratmeterMiete         <- MZsubCA$ef638 
+MZsubCA$Haushaltsgroeße           <- MZsubCA$ef663 
+MZsubCA$AnzahlKinderHH            <- MZsubCA$ef669
+MZsubCA$AnzahlKinderInsges        <- MZsubCA$ef770 
+MZsubCA$AnzahlEinkommensbezieher  <- MZsubCA$ef667 
+
+
+# einzelne Variablen betrachten/umcodieren
+
 install.packages('summarytools')
 library(summarytools)
+library(car)             # umcodieren von Variablen
 
-freq(MZsubCA$ef136) 
-freq(MZsubCA$ef739) # Clusteranalyse lieber auf Haupteinkommensbezieher beziehen? Eigentlich fast sinnvoller, oder?
+MZsubCA$Land <- recode(MZsubCA$Land, "11=2")  #West = 1, Ost = 0
+freq(MZsubCA$Land)
 
-###### VARIABLEN #########################################
-#LandD <- Mikrozensus$ef1
+freq(MZsubCA$BerufHEB)
 
-#### BEFRAGTER
-#Beruf <- Mikrozensus$ef136
-#Wirtschaftszweig <- Mikrozensus$ef137
-#Einkommen <- Mikrozensus$ef830
+MZsubCA$EinkommenHEB <- recode(MZsubCA$EinkommenHEB, "50=NA;90=NA;99=NA") #selbstständiger Landwirt, kein Einkommen, ohne Angabe = NA
+freq(MZsubCA$EinkommenHEB)
 
-#hSchulabschluss <- Mikrozensus$ef310
-#hAbschluss <- Mikrozensus$ef312
-#Geschlecht <- Mikrozensus$ef44
+MZsubCA$AbschlussHEB <- recode(MZsubCA$AbschlussHEB, "11=1;21=2;31=3;32=4;33=5;41=6;51=7;52=8;60=9") #ordinale 9-stufige Skala für Abschluss
+freq(MZsubCA$AbschlussHEB)
 
-#Alter <- Mikrozensus$ef46
-#Arbeitszeit <- Mikrozensus$ef131    # viel NA (52%) - trotzdem reinnehmen?
 
-#### HAUPTEINKOMMENSBEZIEHER
-#BerufHEB <- Mikrozensus$ef739
-#WirtschaftszweigHEB <- Mikrozensus$ef737
-#NettoeinkommenHEB <- Mikrozensus$ef742
+CA <-MZsubCA[,c("Land", "BerufHEB", "EinkommenHEB", "AbschlussHEB", "GeschlechtHEB", "StaatsangehörigkeitHEB",
+                   "Haushaltseinkommen", "Wohnraumgroeße", "QuadratmeterMiete", "Haushaltsgroeße",
+                   "AnzahlKinderHH", "AnzahlKinderInsges", "AnzahlEinkommensbezieher")]
 
-#hSchulabschlussHEB <- Mikrozensus$ef743
-#hAbschlussHEB <- Mikrozensus$ef745
-#GeschlechtHEB <- Mikrozensus$ef731
-
-#AlterHEB <- Mikrozensus$ef         # Gibts nicht?
-#GeschlechtHEB <- Mikrozensus$ef    # Gibts nicht?
-#ArbeitszeitHEB <- Mikrozensus$ef   # Gibts nicht?
-
-#### HAUSHALT
-#Haushaltseinkommen <- Mikrozensus$ef707
-#Wohnraumgröße <- Mikrozensus$ef492
-#QuadratmeterMiete <- Mikrozensus$ef638   # 57% NA - trotzdem interessant?
-#Haushaltsgröße <- Mikrozensus$ef663
-#AnzahlKinderHH <- Mikrozensus$ef669
-#AnzahlKinderInsges <- Mikrozensus$ef770
-#AnzahlEinkommensbezieher <- Mikrozensus$ef667
-#Lebensformenkonzept <- Mikrozensus$ef736   # 4 Lebensformkonzepte, vielleicht auch interessant für eine Untersuchung?
+summary(CA)
+names(CA)
 
 
 
+
+
+# Skalieren
+
+CA1 <- scale(xxxx) #### folgt noch
+
+# Ellbogenkriterium / Silhouette
+
+fviz_nbclust(CA1, hcut, method = "wss") +
+  geom_vline(xintercept = 3, linetype = 2)+
+  labs(subtitle = "Elbow method")
+
+fviz_nbclust(CA1, hcut, method = "silhouette")+
+  labs(subtitle = "Silhouette method")
+
+# Distanzmatrix
+
+d1 <- dist(CA1, method = 'euclidean') # check different distances with: 
+?dist
+
+head(as.matrix(d1))
+
+# Dendrogramm
+
+hc.compl <- hclust(d1, method="complete")
+fviz_dend(hc.compl)
 
 
 
